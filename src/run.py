1d1abefe947cf35299a8bfda63685d46fae0952a
Ayoub Rayane MESBAH <92739091+MesbahAyoubRayane@users.noreply.github.com>
2023-12-06 19:46:31 +0100

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,10 +1,3 @@
-import numpy as np
-import matplotlib
-import ttkbootstrap
-import tkinter
-import tkinter.ttk
-import gui
-import images_function_tools as ift
-
 if __name__ == "__main__":
+    import gui
     gui.Application().run()
