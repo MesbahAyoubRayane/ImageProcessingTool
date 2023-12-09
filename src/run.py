05b39b11ed0e149dd6659d4176e98647b8a47c6e
Ayoub Rayane MESBAH <92739091+MesbahAyoubRayane@users.noreply.github.com>
2023-12-09 21:11:32 +0100

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,3 +1,8 @@
 if __name__ == "__main__":
     import gui
     gui.Application().run()
+
+# this command will allow you to compile thos code to an executable
+"""
+python3 -m nuitka --disable-console --standalone --enable-plugin=tk-inter --enable-plugin=numpy  --enable-plugin=pyqt5 src/run
+"""
